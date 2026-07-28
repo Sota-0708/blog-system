6fd359afd68b42f837955868c2d08865390323cd
広口壮太 <oppeee@wlan-napt-004.u-aizu.ac.jp>
2026-07-28 13:29:49 +0900

    Add login/register, replace hard-coded author with request.user, add search form, closes #5

--- blog/urls.py
+++ blog/urls.py
@@ -5,4 +5,7 @@ urlpatterns = [
     path("", views.post_list, name="post_list"),
     path("posts/<int:post_id>/", views.post_detail, name="post_detail"),
     path("posts/new/", views.post_create, name="post_create"),
+    path("register/", views.register, name="register"),
+    path("login/", views.login_view, name="login"),
+    path("logout/", views.logout_view, name="logout"),
 ]
